a7455d5ed73ba2d747248577bc1a4175d7af5296
Linnea Gräf <nea@nea.moe>
2024-03-26 14:47:50 +0100

    Fix extra \n

--- .github/workflows/generate_descriptions.sh
+++ .github/workflows/generate_descriptions.sh
@@ -11,7 +11,7 @@ for plugin in $(cd plugins/; ls); do
   echo "[Read more](./$plugin/)" >> dist/README.md
 
   echo "# $(jq -r .name < "plugins/$plugin/plugin.json")">>dist/"$plugin"/README.md
-  echo "> **Note:** You are on a plugin page, probably by clicking one of the plugin links. You need to copy the link address and paste it into the shelter settings.\n" >>dist/"$plugin"/README.md
+  echo "> **Note:** You are on a plugin page, probably by clicking one of the plugin links. You need to copy the link address and paste it into the shelter settings." >>dist/"$plugin"/README.md
   echo "" >>dist/"$plugin"/README.md
   echo "Install url: \`https://nea89o.github.io/shelter-plugins/$plugin\`">>dist/"$plugin"/README.md
   echo "" >>dist/"$plugin"/README.md
